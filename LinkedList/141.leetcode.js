3785eb038a8bd68a5ffcccdaed4775565284e775
Satish Sharma <satisharma453@gmail.com>
2026-01-24 11:08:02 +0530

    141 leet code detect cycle in Linked List

---
+++ LinkedList/141.leetcode.js
@@ -0,0 +1,12 @@
+// 141. Linked List Cycle
+
+var hasCycle = function(head) {
+    let slow = head;
+    let fast = head;
+    while (fast != null && fast.next != null){
+        slow = slow.next;
+        fast = fast.next.next;
+        if(fast === slow) return true
+    }
+    return false
+};
