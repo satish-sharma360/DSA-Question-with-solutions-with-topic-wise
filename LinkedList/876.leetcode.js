ed0041f9c5f9909702cb588e86bd31c6eae027d7
Satish Sharma <satisharma453@gmail.com>
2026-01-24 10:36:26 +0530

    create node and linked List class then i perform finMiddle

--- LinkedList/876.leetcode.js
+++ LinkedList/876.leetcode.js
@@ -1,5 +1,51 @@
 //876. Middle of the Linked List
 
+class Node {
+    constructor(data){
+        this.data = data;
+        this.next = null
+    }
+}
+class LinkedList{
+    constructor(){
+        this.head = null
+    }
+    // add node at end
+    append(data){
+        const newNode = new Node(data)
+
+        if(!this.head){
+            this.head = newNode;
+            return
+        }
+
+        let current = this.head
+        while(current.next){
+            current = current.next
+        }
+        current.next = newNode
+    }
+    // find Middle
+    findMiddle(){
+        let slow = this.head;
+        let fast = this.head
+
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next
+        }
+        return slow? slow.data : null
+    }
+}
+
+const list = new LinkedList();
+list.append(10)
+list.append(20)
+list.append(30)
+list.append(40)
+list.append(50)
+
+console.log(list.findMiddle())
 var middleNode = function(head) {
     if(head.next == null) return head
     let slow = head;
