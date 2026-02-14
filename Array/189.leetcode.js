fbd0fbc3b7d86fe83b61fd0bc9fa33293efe693c
Satish Sharma <satisharma453@gmail.com>
2026-02-14 19:33:14 +0530

    solved brut force approach

--- Array/189.leetcode.js
+++ Array/189.leetcode.js
@@ -1,5 +1,29 @@
 // 189. Rotate Array
 
+
+let rotateByD = function(arr , d){
+    let temp = []
+
+    d = d % arr.length;
+    
+    for (let i = 0; i < arr.length; i++) {
+        temp[(i + d)% arr.length] = arr[i]
+        // start from dth postion
+        // d = 4
+        // [nums[0],nums[1] ...]
+    }
+
+    for(let i = 0; i < arr.length; i++){
+        arr[i] = temp[i]
+        // fills starting position 
+        //startng position nothing means at start 
+    }
+    return arr
+}
+
+
+
+
 function reverseArray(nums, i, j) {
     while (i < j) {
         [nums[i], nums[j]] = [nums[j], nums[i]];
@@ -22,4 +46,4 @@ var rotate = function(nums, k) {
     return nums
 };
 
-console.log(rotate([1,2,3,4,5,6,7] , 3))
+console.log(rotateByD([1,2,3,4,5,6,7] , 3))
