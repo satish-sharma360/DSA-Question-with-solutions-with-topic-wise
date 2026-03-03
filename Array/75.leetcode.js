69adcc7ab2d09fb6e1b8c87abfb0d7b6058591e5
Satish Sharma <satisharma453@gmail.com>
2026-03-03 15:55:01 +0530

    optimize some question

--- Array/75.leetcode.js
+++ Array/75.leetcode.js
@@ -1,5 +1,30 @@
 // 75. Sort Colors
 
+
+function SortColors(nums){
+    let count0 = 0;
+    let count1 = 0;
+    let count2 = 0;
+    for(let i = 0; i < nums.length; i++){
+        if(nums[i] === 0) count0++
+        if(nums[i] === 1) count1++
+        if(nums[i] === 2) count2++
+    }
+
+    for(let i = 0; i < count0; i++){
+        nums[i] = 0
+    }
+    for(let i = count0; i < count1 + count0; i++){
+        nums[i] = 1
+    }
+    for(let i = count1 + count0; i < nums.length; i++){
+        nums[i] = 2
+    }
+}
+
+// T.C --> O(2n)
+
+
 var sortColors = function(nums) {
     let start = 0;
     let end = nums.length - 1
@@ -19,3 +44,5 @@ var sortColors = function(nums) {
         }
     }
 };
+
+// T.C -> O(n)
--- Foundation/53.Maximum-Subarray.js
+++ Foundation/53.Maximum-Subarray.js
@@ -1,5 +1,17 @@
 //53.Maximum Subarray.js
 
+function BruteApproach(nums){
+    let max = -Infinity;
+    for(let i = 0; i < nums.length; i++){
+        let sum = 0;
+        for(let j = i; j < nums.length; j++){
+            sum += nums[j]
+            max = Math.max(max , sum)
+        }
+    }
+    return max
+}
+
 function maxSubArraySum(array){
     let max = -Infinity;
     let sum = 0;
