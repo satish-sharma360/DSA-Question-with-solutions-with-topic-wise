73ec2e4d1f555122dc7eba16d2650d5134e9c251
Satish Sharma <satisharma453@gmail.com>
2026-03-09 11:46:20 +0530

    try anothe approach in same problem using stack

--- string/1021.leetcode.js
+++ string/1021.leetcode.js
@@ -22,6 +22,47 @@
 // The input string is "()()", with primitive decomposition "()" + "()".
 // After removing outer parentheses of each part, this is "" + "" = "".
 
+
+
+// ********* Approach One Useing Stack ******************
+function removeOuterParenthesesUsingStack(s){
+    let stack = [];
+    let result = "";
+
+    for(let i = 0; i < s.length; i++){
+        if(s[i] === "("){
+            if(stack.length > 0){
+                result += s[i];
+            }
+            stack.push(s[i]);
+        }else{
+            stack.pop();
+            if(stack.length > 0){
+                result += s[i];
+            }
+        }
+    }
+    return result;
+}
+
+function removeOuter(s){
+    let stack = [];
+    let ans = "";
+
+    for(let i = 0; i< s.length; i++){
+        // agar ( hai push ho jao then agarn ( aaya to length != 0 add into ans and then push again stack now ( ( now come ) simpley pop menas stack now ( and length != 0 and ) into ans 
+        if(s[i] === ')'){
+            stack.pop();
+        }
+        if(stack.length !== 0){
+            ans += s[i];
+        }
+        if(s[i] === '('){
+            stack.push(s[i]);
+        }
+    }
+}
+
 function removeOuterParentheses(s) {
     let result = "";
     let digit = 0;
@@ -44,3 +85,7 @@ function removeOuterParentheses(s) {
 console.log(removeOuterParentheses("(()())(())"))
 console.log(removeOuterParentheses("(()())(())(()(()))"))
 console.log(removeOuterParentheses("()()"))
+
+console.log(removeOuterParenthesesUsingStack("(()())(())"))
+console.log(removeOuterParenthesesUsingStack("(()())(())(()(()))"))
+console.log(removeOuterParenthesesUsingStack("()()"))
