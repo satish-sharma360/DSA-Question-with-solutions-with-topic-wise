c2d05794c0516951e2e91f7e42432ad43ab7f6b3
Satish Sharma <satisharma453@gmail.com>
2026-03-10 21:09:51 +0530

    solve check palindrom with brute and optimal approach

--- LinkedList/234.leetcode.js
+++ LinkedList/234.leetcode.js
@@ -12,21 +12,60 @@
  * @return {boolean}
  */
 var isPalindrome = function(head) {
-    let arr = [];
-    while( head != null){
-        arr.push(head.val);
-        head = head.next
+    // Brute force Approach 
+
+    // let arr = [];
+    // while( head != null){
+    //     arr.push(head.val);
+    //     head = head.next
+    // }
+
+    // let left = 0;
+    // let right = arr.length - 1
+
+    // while(left < right){
+    //     if(arr[left] != arr[right]){
+    //         return false
+    //     }
+    //     left++
+    //     right--
+    // }
+    // return true
+
+    // Optimize Approach
+
+    if(head === null || head.next === null) return true;
+
+    let slow = head;
+    let fast = head;
+
+    // find middle
+    while(fast && fast.next){
+        slow = slow.next;
+        fast = fast.next.next;
     }
 
-    let left = 0;
-    let right = arr.length - 1
+    let curr = slow;
+    let next = null;
+    let prev = null;
 
-    while(left < right){
-        if(arr[left] != arr[right]){
-            return false
+    while(curr){
+        next = curr.next;
+        curr.next = prev;
+        prev = curr;
+        curr = next;
+    }
+
+    let first = head;
+    let second = prev;
+
+    while(second){
+        if(first.val !== second.val){
+            return false;
         }
-        left++
-        right--
+        second = second.next;
+        first = first.next
     }
     return true
+
 };
