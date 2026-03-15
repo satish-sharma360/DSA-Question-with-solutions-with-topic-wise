0008d93b70c764287d52cf3461057f1d43975678
Satish Sharma <satisharma453@gmail.com>
2026-03-15 14:41:20 +0530

    reverse a stack

--- stack/reverse.js
+++ stack/reverse.js
@@ -1,3 +1,90 @@
+class Stack{
+    constructor(size){
+        this.size = size;
+        this.arr = new Array(size);
+        this.top = -1;
+    }
+
+    // push
+    push(value){
+        if(this.top === this.size - 1){
+            console.log("Stack OverFlow")
+            return;
+        }
+        this.top++;
+        this.arr[this.top] =value;
+    }
+
+    pop(){
+        if(this.top === -1){
+            console.log("Stack Under Flow")
+            return
+        }
+        let removed = this.arr[this.top]
+        delete this.arr[this.top]
+        this.top--;
+        return removed
+    }
+
+    insertAtButtom(value){
+        if(this.top === -1){
+            this.push(value)
+            return
+        }
+
+        let temp = this.pop()
+        this.insertAtButtom(value)
+        this.push(temp)
+    }
+
+    reverse(){
+        if(this.top === 0){
+            return
+        }
+        let temp = this.pop()
+
+        this.reverse()
+        this.insertAtButtom(temp) 
+    }
+
+    print(){
+        console.log("************* Stack ***************")
+        console.log(this.arr)
+    }
+}
+
+let stack = new Stack(6)
+
+stack.push(10)
+stack.push(20)
+stack.push(30)
+stack.push(40)
+stack.push(50)
+stack.push(60)
+
+stack.print()
+stack.reverse()
+stack.print()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 let s = [];
 s.push(10);
@@ -5,6 +92,7 @@ s.push(20);
 s.push(30);
 s.push(40);
 s.push(50);
+
 
 
 function insertAtButtom(s , totalSize, top){
@@ -28,7 +116,7 @@ function reverse(stack){
 
     insertAtButtom(stack , stack.length , temp)
 }
-console.log(s)
+// console.log(s)
 
 reverse(s)
-console.log(s)
+// console.log(s)
