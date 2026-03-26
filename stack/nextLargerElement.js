238a0da34b13198b770862e0808993f37f8234fd
Satish Sharma <satisharma453@gmail.com>
2026-03-26 15:27:56 +0530

    solved same question with better approach

--- stack/nextLargerElement.js
+++ stack/nextLargerElement.js
@@ -14,11 +14,34 @@ function nextLargerElement(arr){
     }
     return result;
 }
-console.log(nextLargerElement([1, 3, 2, 4])) // [3, 4, 4, -1]
-console.log(nextLargerElement([6, 8, 0, 1, 3])) // [8, -1, 1, 3, -1]
-console.log(nextLargerElement([1, 2, 3, 5])) // [ 2, 3, 5, -1 ]
+// console.log(nextLargerElement([1, 3, 2, 4])) // [3, 4, 4, -1]
+// console.log(nextLargerElement([6, 8, 0, 1, 3])) // [8, -1, 1, 3, -1]
+// console.log(nextLargerElement([1, 2, 3, 5])) // [ 2, 3, 5, -1 ]
 
 // T.C -> O(n^2)
 
 // Better Approach
 
+function BetterApproach(arr){
+    let stack = [];
+    let result = [];
+
+    for(let i = arr.length - 1; i >= 0; i--){
+        while(stack.length > 0 && stack[stack.length - 1] <= arr[i]){
+            stack.pop();
+        }
+        if (stack.length === 0) {
+            result[i] = -1;
+        }else{
+            result[i] = stack[stack.length - 1];
+        }
+        stack.push(arr[i])
+    }
+    return result;
+}
+
+console.log(BetterApproach([1, 3, 2, 4])) // [3, 4, 4, -1]
+console.log(BetterApproach([6, 8, 0, 1, 3])) // [8, -1, 1, 3, -1]
+console.log(BetterApproach([1, 2, 3, 5])) // [ 2, 3, 5, -1 ]
+
+// T.C -> O(n)
