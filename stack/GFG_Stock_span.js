67392db2ee3509362ea6b6ffce872d0838101067
Satish Sharma <satisharma453@gmail.com>
2026-03-26 15:11:26 +0530

    solve GFG Stock span problem

---
+++ stack/GFG_Stock_span.js
@@ -0,0 +1,64 @@
+// Stock span problem
+
+/*
+The stock span problem is a financial problem where we have a series of daily price quotes for a stock and we need to calculate the span of stock price for all days.
+You are given an array arr[] representing daily stock prices, the stock span for the i-th day is the number of consecutive days up to day i (including day i itself) for which the price of the stock is less than or equal to the price on day i. Return the span of stock prices for each day in the given sequence.
+
+Examples:
+
+Input: arr[] = [100, 80, 90, 120]
+Output: [1, 1, 2, 4]
+Explanation: Traversing the given input span 100 is greater than equal to 100 and there are no more days behind it so the span is 1, 80 is greater than equal to 80 and smaller than 100 so the span is 1, 90 is greater than equal to 90 and 80 so the span is 2, 120 is greater than 90, 80 and 100 so the span is 4. So the output will be [1, 1, 2, 4].
+
+Input: arr[] = [10, 4, 5, 90, 120, 80]
+Output: [1, 1, 2, 4, 5, 1]
+Explanation: Traversing the given input span 10 is greater than equal to 10 and there are no more days behind it so the span is 1, 4 is greater than equal to 4 and smaller than 10 so the span is 1, 5 is greater than equal to 4 and 5 and smaller than 10 so the span is 2, and so on. Hence the output will be [1, 1, 2, 4, 5, 1].
+
+*/
+
+function StockSpam(stockes){
+    let result = [];
+
+    for(let i = 0; i < stockes.length; i++){
+        let count = 1;
+
+        // Go LEFT OF THE CURRENT ELEMENT
+        for(let j = i - 1; j >= 0; j--){
+            if(stockes[i] >= stockes[j]){
+                count++;
+            }else{
+                break;
+            }
+        }
+        result.push(count)
+    }
+    return result;
+}
+
+// console.log(StockSpam([100, 80, 90, 120])) // [ 1, 1, 2, 4 ]
+// console.log(StockSpam([10, 4, 5, 90, 120, 80])) // [ 1, 1, 2, 4, 5, 1 ]
+
+// T.C -> O(n^2)
+
+// I use stack and what i do i just user push and pop i pust only index idx
+
+function BetterApproach(arr){
+    let stack = [];
+    let result = [];
+
+    for(let i = 0; i < arr.length; i++){
+        while (stack.length > 0 && arr[stack[stack.length - 1]] < arr[i]) {
+            stack.pop()
+        }
+        if (stack.length === 0) {
+            result.push(i + 1)
+        }else{
+            result.push ( i - stack[stack.length - 1])
+        }
+        stack.push(i)
+    }
+    return result;
+}
+
+console.log(BetterApproach([100, 80, 90, 120])) // [ 1, 1, 2, 4 ]
+console.log(BetterApproach([10, 4, 5, 90, 120, 80])) // [ 1, 1, 2, 4, 5, 1 ]
