c383c032ac75a4dd37165654365fa209ab961c84
Satish Sharma <satisharma453@gmail.com>
2026-04-03 10:40:44 +0530

    solved 387 leetcode problem

--- temp.js
+++ temp.js
@@ -1,83 +1,17 @@
+function Solve(arr){
+    let set = new Set()
 
-class Queue{
-    constructor(size){
-        this.size = size;
-        this.queue = new Array(size);
-        this.front = -1;
-        this.rear = -1;
-    }
-
-    isFull(){
-        return (this.rear + 1) % this.size === this.front;
-    }
-
-    push(val){
-        if (this.isFull()) {
-            console.log("Queu is full")
-            return;
-        }
-        // push first val
-        if (this.front === -1) {
-            this.front++;
-            this.rear++
+    for(let i = 0; i < arr.length; i++){
+        if (set.has(arr[i])) {
+            set.delete(arr[i])
         }else{
-            this.rear = (this.rear + 1) % this.size;
+            set.add(arr[i])
         }
-        this.queue[this.rear] = val;
-        return;
     }
-
-    pop(){
-        if (this.front === -1) {
-            console.log("Queue is Empty")
-            return;
-        }
-        let removed = this.queue[this.front];
-        if (this.front === this.rear) {
-            this.front = -1;
-            this.rear = -1;
-        }else{
-            this.front = (this.front + 1) % this.size;
-        }
-        
-        return removed;
-    }
-
-    peek(){
-        return this.queue[this.front]
-    }
-
-    print(){
-        if (this.front === -1) {
-            console.log("Queue is Empty")
-            return;
-        }
-
-        let i = this.front;
-        let result = "";
-
-        while(true){
-            result += this.queue[i] + " ";
-            if (i == this.rear) {
-                break;
-            }
-            i = (i + 1) % this.size;
-        }
-        console.log(result)
-    }
+    return set;
 }
 
-let q = new Queue(5);
+console.log(Solve([10,20,30,30,20]))
 
-q.push(10)
-q.push(20)
-q.push(30)
-q.push(40)
-q.pop()
-q.pop()
-q.push(50)
-q.push(60)
-q.push(70)
-q.print()
 
 // console.log(25 + 13 + 5 + 20 + 3 + 18 + 10 + 3)
