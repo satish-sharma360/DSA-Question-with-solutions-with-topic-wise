b6a896e4b7acc5d87aa6784acb272f0ebdb1dba0
Satish Sharma <satisharma453@gmail.com>
2026-04-10 18:26:04 +0530

    find height of a tree recursive

---
+++ Tree/Binary_Tree/height.js
@@ -0,0 +1,53 @@
+// Find Height of a Binary Tree
+
+class Node{
+    constructor(data){
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class BinaryTree{
+    constructor(){
+        this.index = -1;
+    }
+    buildTree(arr){
+        // index increment
+        this.index++;
+
+        // base case
+        if (arr[this.index] === -1) {
+            return null;
+        }
+
+        // Create Root
+        let root = new Node(arr[this.index]);
+
+        // Create Left sub Tree;
+        root.left = this.buildTree(arr);
+
+        // Create Left sub Tree;
+        root.right = this.buildTree(arr);
+
+        return root;
+    }
+}
+
+function findHeight(root){
+
+    if (root === null) {
+        return 0;
+    }
+
+    let leftHeight = findHeight(root.left);
+    let rightHeight = findHeight(root.right);
+
+    return Math.max(leftHeight , rightHeight) + 1;
+} 
+
+let arr = [1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1];
+
+let tree = new BinaryTree()
+let root = tree.buildTree(arr);
+console.log(findHeight(root))
